02293c20c7464585f432155199253dc0baf79954
cosimobassi646 <bassi.cosimo@itismeucci.com>
2024-10-11 19:06:14 +0200

    commit a casa

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -12,17 +13,31 @@ public class Main {
         System.out.println("client partito!");
         Socket s = new Socket("localhost", 3000);
         System.out.println("il client si è collegato");
-        do{
-        System.out.println("scrivi la stringa da passare(scrivi '!' per terminare le operazioni): ");
-        Scanner in = new Scanner( System.in );
-        String stringaRicevuta = in.nextLine();
-        if (stringaRicevuta.equals("!")) {
-            break;
-        }
-        System.out.println("la stringa che hai passato è: " + stringaRicevuta);
-        }while(true);
-        
-        
-        
+
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        BufferedReader inDalServer = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        Scanner in = new Scanner(System.in);
+
+        do {
+            //input della stringa
+            System.out.println("scrivi la stringa da passare (scrivi '!' per terminare le operazioni): ");
+            String stringaDaInviare = in.nextLine();
+
+            if (stringaDaInviare.equals("!")) {
+                out.writeBytes(stringaDaInviare + "\n");
+                break;
+            }
+
+            // Invia la stringa al server
+            out.writeBytes(stringaDaInviare + "\n");
+
+            // risposta dal server
+            String stringaRicevuta = inDalServer.readLine();
+            System.out.println("Risposta dal server: " + stringaRicevuta);
+        } while (true);
+
+        s.close();
+        in.close();
+        System.out.println("FINE CONNESSIONE");
     }
 }
